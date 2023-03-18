0db63a053777323d2bf566c990e61a9fa96660c0
guest400123064 <lgfz1003@icloud.com>
2023-03-18 13:16:09 -0400

    .

--- scripts/download_benchmark.py
+++ scripts/download_benchmark.py
@@ -16,29 +16,27 @@ import logging
 
 from beir import util, LoggingHandler
 
-
 logging.basicConfig(format='%(asctime)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.INFO,
                     handlers=[LoggingHandler()])
 
-
-DATASET_NAMES = ["trec-covid",
-                 "nfcorpus",
-                 "nq",
-                 "hotpotqa",
-                 "fiqa",
-                 "scidocs",
-                 "arguana",
-                 "quora",
-                 "scifact"]
+DATASETS = ["trec-covid",
+            "nfcorpus",
+            "nq",
+            "hotpotqa",
+            "fiqa",
+            "scidocs",
+            "arguana",
+            "quora",
+            "scifact"]
 
 
 if __name__ == "__main__":
 
     out_dir = pathlib.Path(__file__).parent.parent.absolute() / "data" / "datasets"
     url_fmt = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{name}.zip"
-    for name in tqdm.tqdm(DATASET_NAMES):
+    for name in tqdm.tqdm(DATASETS):
         url = url_fmt.format(name=name)
         util.download(url, out_dir / "zip")
         util.unzip(out_dir / "zip" / f"{name}.zip", out_dir / "raw")
